9f478585ef9caed746cd085eb89467be14f96318
Ambrish Gupta <ambrishg293@gmail.com>
2026-07-01 08:43:23 +0530

    Time: 0 ms (100%), Space: 43.4 MB (76.64%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,28 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-        ArrayList<Integer> zeros = new ArrayList<>();
-                ArrayList<Integer> ones = new ArrayList<>();
-        ArrayList<Integer> twos = new ArrayList<>();
         int n = nums.length;
-    for(int i = 0 ; i<n;i++){
-        if(nums[i]==0){
-            zeros.add(nums[i]);
-        }else if(nums[i]==1){
-            ones.add(nums[i]);
-        }else if(nums[i]==2){
-            twos.add(nums[i]);
+        // int cnt  = 1;
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
+
+        for(int i = 0 ;i<n;i++){
+            if(nums[i]==0){
+                zeros++;
+            }else if(nums[i]==1){
+                ones++;
+            }else if(nums[i]==2){
+                twos++;
+            }
+
+            }
+            for(int i = 0 ;i<n;i++){
+                if(i<zeros){
+                    nums[i]=0;
+                }else if(i<zeros+ones){
+                    nums[i]=1;
+                }else{
+                    nums[i]=2;
+                }
+            }
         }
     }
-    int k=0;
-    for(int i = 0; i<zeros.size();i++){
-        nums[k] = zeros.get(i);
-        k++;
-}   for(int i = 0; i<ones.size();i++){
-        nums[k] = ones.get(i);
-        k++;
-}   for(int i = 0; i<twos.size();i++){
-        nums[k] = twos.get(i);
-        k++;
-}
-    }
-}
