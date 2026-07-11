4262e9b9461c0e32b1600e9da1a9640e0416bc46
Ambrish Gupta <ambrishg293@gmail.com>
2026-07-11 17:11:10 +0530

    Time: 0 ms (100%), Space: 43.5 MB (23.49%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] plusOne(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (arr[i] != 9) {
+                arr[i] = arr[i] + 1;
+                return arr;
+            }
+
+            arr[i] = 0;
+        }
+
+        int[] arr1 = new int[n + 1];
+        arr1[0] = 1;
+
+        return arr1;
+    }
+}
