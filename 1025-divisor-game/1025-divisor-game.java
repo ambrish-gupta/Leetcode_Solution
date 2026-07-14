d7a0a9ba70df474d6f9c8a0386bc56ef95f5c298
Ambrish Gupta <ambrishg293@gmail.com>
2026-07-14 13:14:32 +0530

    Time: 0 ms (100%), Space: 41.9 MB (85.86%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean divisorGame(int n) {
-        if(n%2 == 0){
-            return true ;
-        }else{
-            return false;
-        }
+        return n%2 == 0;
     }
 }
