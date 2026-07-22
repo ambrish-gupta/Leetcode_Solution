955b3b283bd1a8b4da6f304cb18f81635eb3910c
Ambrish Gupta <ambrishg293@gmail.com>
2026-07-22 22:56:43 +0530

    Time: 0 ms (100%), Space: 43.1 MB (68.14%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
@@ -1,39 +1,35 @@
 class Solution {
     public int matrixScore(int[][] arr) {
-        int m = arr.length, n = arr[0].length;
-        for (int i = 0; i < m; i++) {
-            if (arr[i][0] == 0) {
-                for (int j = 0; j < n; j++) {
-                    if (arr[i][j] == 0)
-                        arr[i][j] = 1;
-                    else
-                        arr[i][j] = 0;
-                }
+       int m = arr.length, n = arr[0].length;
+       for(int i = 0; i<m;i++){
+        if(arr[i][0]==0){
+            for(int j = 0;j<n;j++){
+                if(arr[i][j]==0)arr[i][j]=1;
+                else arr[i][j]=0;
             }
         }
-        for (int j = 1; j < n; j++) {
-            int nozeros = 0, noones = 0;
-            for(int i = 0;i<m;i++){
-                if(arr[i][j]==0)nozeros++;
-                else noones++;
-            }
-            if(nozeros>noones){
-                for(int i = 0; i<m;i++){
-                     if (arr[i][j] == 0)
-                        arr[i][j] = 1;
-                    else
-                        arr[i][j] = 0;
-                }
+       } 
+       for(int j = 1; j<n;j++){
+        int noz = 0, noO = 0;
+        for(int i = 0; i<m; i++){
+            if(arr[i][j]==0)noz++;
+            else noO++;
+        }
+        if(noz>noO){
+            for(int i = 0; i<m;i++){
+                if(arr[i][j]==0)arr[i][j]=1;
+                else arr[i][j]=0;
             }
         }
-        int score = 0;
-         int x = 1;
-            for(int j = n-1; j>=0;j--){
-                for(int i = 0; i<m;i++){
-                score+= (arr[i][j]*x);
-            }
-            x*=2;
-         }
-         return score;
+       }
+       int res = 0; 
+       int y = 1;
+       for(int j = n-1;j>=0;j--){
+        for(int i = 0; i<m;i++){
+            res+=(arr[i][j]*y);
+        }
+        y*=2;
+       }
+       return res;
     }
 }
